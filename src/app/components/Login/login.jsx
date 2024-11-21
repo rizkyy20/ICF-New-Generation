8a8af3c84b4f16ad4aeac88b8cbe21251fa481e3
rizkyy20 <muhammadrizkyyamin@gmail.com>
2024-11-21 23:27:31 +0800

    Permulaan

---
+++ src/app/components/Login/login.jsx
@@ -0,0 +1,14 @@
+
+const Login = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="bg-gray-500 rounded border border-solid border-blue">
+                <div>
+                    <h1>Login</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+
+import Login from "./components/Login/login"
+
+const Page = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default Page
